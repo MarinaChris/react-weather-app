45f7f52794b1700e4d23f66779ea4c13adaf7748
Marina Christodoulou <kmchris09@gmail.com>
2021-08-09 15:43:56 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       Weather App
       <footer>
         This project was coded by <a href="https://github.com/" target="_blank" rel="nonreferrer">Marina Chris</a> and is open-sourced on <a href="https://github.com/MarinaChris/react-weather-app" target="_blank" rel="nonreferrer">
         Github
         </a>
       </footer>
+      </div>
     </div>
   );
 }
