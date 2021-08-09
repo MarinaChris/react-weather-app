647b2c5a3fff28eacfc89bd34b58b30c92530710
Marina Christodoulou <kmchris09@gmail.com>
2021-08-09 16:10:06 +0200

    Basic setup and adding search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-      Weather App
+      <Weather />
       <footer>
         This project was coded by <a href="https://github.com/" target="_blank" rel="nonreferrer">Marina Chris</a> and is open-sourced on <a href="https://github.com/MarinaChris/react-weather-app" target="_blank" rel="nonreferrer">
         Github
