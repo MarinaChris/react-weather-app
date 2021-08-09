2b897c52137052c2260beec16a1a6443e44a3861
Marina Christodoulou <kmchris09@gmail.com>
2021-08-09 15:30:00 +0200

    Added Footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by <a href="https://github.com/" target="_blank" rel="nonreferrer">Marina Chris</a> and is open-sourced on <a href="https://github.com/MarinaChris/react-weather-app" target="_blank" rel="nonreferrer">
+        Github
+        </a>
+      </footer>
     </div>
   );
 }
